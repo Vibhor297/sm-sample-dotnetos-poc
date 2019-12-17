2fe75bf96f6028c911041e655f1f7b7b598ae408
Vibhor Arora <Vibhor.Arora@det.nsw.edu.au>
2019-12-17 13:54:41 +1100

    first commit

---
+++ sample-openshift-dotnet-poc/Program.cs
@@ -0,0 +1,121 @@
+﻿using Amazon.DynamoDBv2;
+using Hangfire;
+using Newtonsoft.Json;
+using Quartz;
+using Quartz.Impl;
+using Sample.Repository.Models;
+using Sample.Services;
+using sample_openshift_dotnet_poc.Static;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sample_openshift_dotnet_poc
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            try
+            {
+                //NameValueCollection nameValueCollection = new NameValueCollection {
+                //    {"quartz.threadPool.type", "Quartz.Simpl.SimpleThreadPool, Quartz" },
+                //    { "quartz.threadPool.threadCount","10" },
+                //    { "quartz.threadPool.threadPriority", "2" }
+                //};
+
+                StdSchedulerFactory factory = new StdSchedulerFactory();
+
+
+                // Grab the Scheduler instance from the Factory 
+                IScheduler scheduler = await factory.GetScheduler();
+
+                // and start it off
+                await scheduler.Start();
+
+                // define the job and tie it to our HelloJob class
+                IJobDetail job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+                // Trigger the job to run now, and then repeat every 10 seconds
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(2)
+                        )
+                    .Build();
+
+                // Tell quartz to schedule the job using our trigger
+                await scheduler.ScheduleJob(job, trigger);
+
+                // some sleep to show what's happening
+                Thread.Sleep(TimeSpan.FromSeconds(60));
+
+                // and last shut down the scheduler when you are ready to close your program
+                await scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+
+            Console.WriteLine("Press any key to close the application");
+            Console.ReadKey();
+        }
+
+    }
+
+
+    public class HelloJob : IJob
+    {
+        async Task IJob.Execute(IJobExecutionContext context)
+        {
+            using (StudentService studentService = new StudentService())
+            {
+
+                Program program = new Program();
+
+                List<SepsdChangedStudent> changedStudents = await studentService.GetChangedStudents();
+
+                //List<SepsdStudent> students = await studentService.LoadStudent(new SepsdChangedStudent());
+
+                //var studentJson = JsonConvert.SerializeObject(students);
+
+                //PutItemResponse response = await Common.putItemAsync(client, "447571773", "", studentJson);
+
+                //Console.WriteLine(response.HttpStatusCode);
+
+                if (changedStudents.Count > 0)
+                {
+                    using (AmazonDynamoDBClient client = Common.GetDynamodbClient())
+                    {
+                        if (client != null)
+                        {
+                            TaskFactory taskFactory = new TaskFactory();
+                            //get students from ern
+                            foreach (var studentChanged in changedStudents)
+                            {
+                                var response = await taskFactory.StartNew(async () =>
+                                {
+                                    List<SepsdStudent> students = await studentService.LoadStudent(studentChanged);
+
+                                    var studentJson = JsonConvert.SerializeObject(students);
+
+                                    await Common.putItemAsync(client, studentChanged.StudentRecordNo.ToString(), "Student_Entity", studentJson);
+
+                                });
+                                Task.WaitAll(response);                                
+                            }
+                        }
+                        else
+                            Console.WriteLine("Error in creating AWS dynamo db session");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sample-openshift-dotnet-poc/Static/Common.cs
@@ -0,0 +1,67 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace sample_openshift_dotnet_poc.Static
+{
+    public static class Common
+    {
+        public static AmazonDynamoDBClient GetDynamodbClient()
+        {
+            AmazonDynamoDBClient client = null;
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var chain = new CredentialProfileStoreChain();
+            AWSCredentials awsCredentials;
+            if (chain.TryGetAWSCredentials("saml", out awsCredentials))
+            {
+                var credentials = awsCredentials.GetCredentials();
+
+                client = new AmazonDynamoDBClient(credentials.AccessKey, credentials.SecretKey, credentials.Token, Amazon.RegionEndpoint.APSoutheast2);
+
+                //string strTableName = "StudentDetails";
+
+                //var request = new GetItemRequest
+                //{
+                //    TableName = strTableName,
+                //    Key = new Dictionary<string, AttributeValue>() { { "StudentSRN", new AttributeValue { S = "1001" } } },
+                //};
+
+                //var response = client.GetItemAsync(request);
+
+                //// Check the response.
+                //var result = response.Result;
+            }
+
+            return client;
+        }
+
+        public static System.Threading.Tasks.Task<PutItemResponse> putItemAsync(AmazonDynamoDBClient amazonDynamoDBClient, string srnNumber, string entity, string json_value)
+        {
+            string strTableName = "StudentDetails";
+            
+            var request = new PutItemRequest
+            {
+                TableName = strTableName,
+                Item = new Dictionary<string, AttributeValue>()
+                    {
+                        { "StudentSRN", new AttributeValue {S = srnNumber} },
+                        { entity, new AttributeValue {S = json_value } }
+                    },
+
+            };            
+
+            return amazonDynamoDBClient.PutItemAsync(request);
+        }
+    }
+}
